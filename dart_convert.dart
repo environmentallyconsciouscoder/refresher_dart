7a30bbd9550731fdf8d6e950abac62fcaad9632d
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-06-30 13:50:32 +0100

    chore: added dart_convert and dart_user_input files

---
+++ dart_convert.dart
@@ -0,0 +1,19 @@
+void main() {
+  // string to int
+  var a, b, c;
+  a = 40;
+  b = "1";
+  c = a + int.parse(b);
+  print("$a + $b = $c");
+
+  // string to double
+  var d, e, f;
+  d = 40;
+  e = "0.1";
+  f = d + double.parse(e);
+
+  var g, h, i;
+  g = 40.1;
+  h = '1';
+  i = g.toString() + h;
+}
