008fc825e77c27e68a430f64bca471d2ec1e55e8
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-06-30 12:19:18 +0100

    chore: add dart_loop.dart

---
+++ dart_loop.dart
@@ -0,0 +1,19 @@
+void main() {
+  // for loop
+  var num = 5;
+  for (var i = num; i >= 1; i--) {
+    print(i);
+  }
+
+  // for in loop
+  var names = ["John", "tina", "steve"];
+  for (var name in names) {
+    print(name);
+  }
+
+  // while loop
+  while (num >= 1) {
+    print(num);
+    num--;
+  }
+}
