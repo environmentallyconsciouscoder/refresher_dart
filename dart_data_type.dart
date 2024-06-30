4a6d2c46bd17797a930b32a889a4c9ac1b327daf
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-06-30 05:33:51 +0100

    chore: add dart_data_type.dart

---
+++ dart_data_type.dart
@@ -0,0 +1,18 @@
+void main() {
+  // data types: numbers, strings, boolean, lists, maps, dynamic
+
+  String firstName = "John";
+  print("String: $firstName");
+
+  int myNum = 41;
+  print("Integer: $myNum");
+
+  double otherNum = 1.5;
+  print("double: $otherNum");
+
+  bool myBool = false;
+  print("boolean: $myBool");
+
+  dynamic fullName = "John smith";
+  print("dynamic: $fullName");
+}
